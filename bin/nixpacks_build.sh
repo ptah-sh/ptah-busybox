73de48c6ef50112c1e6f72506de4c700d615ef38
Bohdan Shulha <bv.shulha@gmail.com>
2024-11-08 13:06:01 +0100

    feat: #12 support builds with nixpacks

---
+++ bin/nixpacks_build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+. "$(dirname "$0")/../lib/validate.sh"
+. "$(dirname "$0")/../lib/sync.sh"
+
+
+check_var TARGET_DIR
+check_var NIXPACKS_FILE_PATH
+check_var IMAGE_NAME
+
+# Create a temporary build directory
+BUILD_DIR=$(mktemp -d)
+trap 'rm -rf "$BUILD_DIR"' EXIT
+
+function copy_files() {
+    cp -r "$TARGET_DIR"/* "$BUILD_DIR"
+}
+
+with_lock "${TARGET_DIR}" copy_files
+
+# Change to the build directory
+cd "$BUILD_DIR"
+
+nixpacks build --config "$NIXPACKS_FILE_PATH" --name "$IMAGE_NAME" .
+
+# Push the image to the registry
+docker push "${IMAGE_NAME}"
