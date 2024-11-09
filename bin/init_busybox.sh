6cfc26dec6cb0f1e9948628750ae3b83857121bc
Bohdan Shulha <bv.shulha@gmail.com>
2024-11-09 13:10:06 +0100

    fix: #17 env var name typo

--- bin/init_busybox.sh
+++ bin/init_busybox.sh
@@ -5,8 +5,8 @@ set -e
 # Source the shared library
 . "$(dirname "$0")/../lib/validate.sh"
 
-check_var PATH_SSH_PUBLIC_KEY
-check_var PATH_SSH_PRIVATE_KEY
+check_var PTAH_SSH_PUBLIC_KEY
+check_var PTAH_SSH_PRIVATE_KEY
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -16,8 +16,8 @@ Host *
   StrictHostKeyChecking accept-new
 EOF
 
-echo "$PATH_SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub
-echo "$PATH_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+echo "$PTAH_SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub
+echo "$PTAH_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
